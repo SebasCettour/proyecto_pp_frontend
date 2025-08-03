829726c1b16660d52e1cb617e1cf9bd952c6feb3
Sebastián Cettour <sebas.cettour83@gmail.com>
2025-08-03 15:59:48 -0300

    Creando Portal empleados y solicitud de licencias. Correcciones y mejoras varias

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -4,6 +4,8 @@ import PublicarNovedades from "../pages/RRHH/PublicarNovedades";
 import { RRHHPrincipal } from "../pages/RRHH/RRHH-Principal";
 import GestionarLicencias from "../pages/RRHH/GestionarLicencias";
 import Tablon from "../pages/RRHH/Tablon";
+import { Empleados } from "../pages/empleados/Empleados";
+import SolicitarLicencia from "../pages/empleados/SolicitarLicencia";
 
 export function AppRoutes() {
   return (
@@ -13,6 +15,8 @@ export function AppRoutes() {
       <Route path="/rrhh-principal" element={<RRHHPrincipal />} />
       <Route path="/licencias" element={<GestionarLicencias />} />
       <Route path="/tablon" element={<Tablon />} />
+      <Route path="/empleados" element={<Empleados />} />
+      <Route path="/solicitar-licencia" element={<SolicitarLicencia />} />
     </Routes>
   );
 }
