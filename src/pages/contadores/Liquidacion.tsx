1cc267f8485379b55dd1e180054944b5a572bdd9
Sebastián Cettour <sebas.cettour83@gmail.com>
2025-08-05 23:43:38 -0300

    Creando Formulario de Liquidacion

---
+++ src/pages/contadores/Liquidacion.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Typography, Box, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import Footer from "../../components/Footer";
+
+const MiComponente = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Box
+      sx={{
+        minHeight: "100vh",
+        backgroundColor: "#C0C0C0",
+        display: "flex",
+        flexDirection: "column",
+        overflowX: "hidden",
+      }}
+    >
+      {/* Encabezado */}
+      <Box
+        sx={{
+          textAlign: "left",
+          py: 4,
+          backgroundColor: "#000000",
+          boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
+          width: "100%",
+          boxSizing: "border-box",
+        }}
+      >
+        <Typography
+          variant="h2"
+          sx={{
+            fontFamily: "Tektur, sans-serif",
+            fontWeight: 700,
+            color: "#333",
+            marginLeft: "10px",
+            userSelect: "none",
+          }}
+        >
+          <span style={{ color: "#CC5500" }}>360</span>
+          <span style={{ color: "#ffffff" }}>Sueldos</span>
+        </Typography>
+      </Box>
+
+      <Footer />
+    </Box>
+  );
+};
+
+export default MiComponente;
